58057f03d038b9818931702a1853cf9f46789b8a
Akshit Tripathi <tripathisoft@gmail.com>
2018-09-16 02:42:56 -0400

    Modal now works. Clicking a button adds the element to the query list.

--- public/home.js
+++ public/home.js
@@ -1,6 +1,13 @@
+// Top 10 predictions considered
+const NUM_MAX_RESPONSES = 10;
+
+const modal = document.getElementById("modal");
+
 const clarifai = new Clarifai.App({
     apiKey: 'c56d788447e649a9898cab58c3058774'
 });
+
+let clarifai_query = [];
 
 function encodeImageFileAsURL(element) {
     var file = element.files[0];
@@ -12,15 +19,34 @@ function encodeImageFileAsURL(element) {
         let img = document.createElement("img");
         img.src = reader.result;
         imageHolder.appendChild(img);
-        console.log('READER', reader.result.substr(23));
 
         clarifai.models.predict("bd367be194cf45149e75f01d59f77ba7", {
             base64: reader.result.substr(23)
         }).then(
             function (response) {
-                response.outputs[0].data.concepts.forEach(element => {
+                let selector = document.getElementById("pred_select");
+                for (let i = 0; i < Math.min(NUM_MAX_RESPONSES, response.outputs[0].data.concepts.length); i++) {
+                    let element = response.outputs[0].data.concepts[i].name;
                     console.log(element);
-                });
+                    let choice = document.createElement("button");
+                    choice.addEventListener("click", () => {
+                        clarifai_query.push(element);
+                        let selector = document.getElementById("pred_select");
+
+                        while (selector.hasChildNodes())
+                            selector.removeChild(selector.firstChild);
+
+                        console.log(clarifai_query);
+                        modal.style.display = "none";
+                    });
+                    choice.value = element;
+                    choice.innerText = element.replace(/^\w/, c => c.toUpperCase());
+
+                    selector.appendChild(choice);
+                }
+
+                modal.style.display = "flex";
+
             },
             function (err) {
                 alert("ERROR:\n" + err);
